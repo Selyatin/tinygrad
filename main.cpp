a94f74d245153dd836bcb546c5260d37911083b1
nikoreun <nikoreunanen@gmail.com>
2016-04-26 08:39:48 +0300

    Refactored and commented the example main code.

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include "node.h"
 #include "utils.h"
 #include "models.h"
-#include "dataset.h"
 
+// fRand originates from http://stackoverflow.com/questions/2704521/generate-random-double-numbers-in-c
 double fRand(double fMin, double fMax){
     double f = (double)rand() / RAND_MAX;
     return fMin + f * (fMax - fMin);
@@ -17,29 +17,32 @@ int main() {
     std::cout << "Evaluating an implementation of logistic regression" << std::endl;
     unsigned int n_features = 4;
     ClassifierLogisticRegression clf(n_features);
-    Tensor *t1 = create_guarded_tensor_with_random_elements(1, n_features, 0.0, 2.0); // input);
+    Tensor *input = create_guarded_tensor_with_random_elements(1, n_features, 0.0, 2.0); // input);
 
     int repeats = 100;
     double target;
     while(repeats > 0) {
+        // Create a random input (x) and target label (y) for classification (two classes).
         if (repeats % 2 == 0){
-            for(int i=0;i<(t1->rows*t1->cols);i++){
-                t1->data[i] = fRand(0.0, 1.0);
+            for(int i=0;i<n_features;i++){
+                input->data[i] = fRand(0.0, 1.0);
             }
             target = 1.0;
         } else {
-            for(int i=0;i<(t1->rows*t1->cols);i++){
-                t1->data[i] = fRand(-1.0, 0.0);
+            for(int i=0;i<n_features;i++){
+                input->data[i] = fRand(-1.0, 0.0);
             }
             target = 0.0;
         }
 
+        // Evaluate a given input
         std::cout << "f(";
-        print_tensor_as_eigen_matrix(t1, false);
+        print_tensor_as_eigen_matrix(input, false);
         std::cout << ") = ";
-        print_tensor_as_eigen_matrix(clf.evaluate(t1), true);
+        print_tensor_as_eigen_matrix(clf.evaluate(input), true);
 
-        clf.sgd(t1, target, 0.1);
+        // Compute stochastic gradient descend with a given learning rate
+        clf.sgd(input, target, 0.1);
 
         repeats--;
     }
